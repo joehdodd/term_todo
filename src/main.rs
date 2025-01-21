e070458f67d82c6d390ce6ec6c276e5b006f6f73
Joe Dodd <joehdodd@gmail.com>
2025-01-21 14:14:35 -0500

    Implements ratatui, crossterm and tui-input.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,56 @@
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::layout::{Constraint, Direction, Layout};
 use ratatui::{
     buffer::Buffer,
-    layout::Rect,
+    layout::{Position, Rect},
     style::{Color, Style, Stylize},
     symbols::border,
     text::Line,
-    widgets::{Block, List, ListDirection, ListState, StatefulWidget},
+    widgets::{Block, List, ListDirection, ListState, Paragraph, StatefulWidget},
     DefaultTerminal, Frame,
 };
 use std::io;
+use tui_input::backend::crossterm::EventHandler;
+use tui_input::Input;
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
     let mut app = App {
         exit: false,
-        selected: 0,
+        todos: vec![
+            Todo {
+                desc: "Learn Rust".to_string(),
+                done: false,
+            },
+            Todo {
+                desc: "Iterate on Term_Todo".to_string(),
+                done: false,
+            },
+        ],
+        input: Input::default(),
+        input_mode: InputMode::Normal,
     };
     let app_result = app.run(&mut terminal);
     ratatui::restore();
     app_result
 }
 
+enum InputMode {
+    Normal,
+    Insert,
+}
+
 pub struct App {
     exit: bool,
-    selected: usize,
+    todos: Vec<Todo>,
+    input: Input,
+    input_mode: InputMode,
+}
+
+#[derive(Clone)]
+struct Todo {
+    desc: String,
+    done: bool,
 }
 
 impl App {
@@ -39,7 +66,34 @@ impl App {
     }
 
     fn draw(&self, frame: &mut Frame, list_state: &mut ListState) {
-        frame.render_stateful_widget(self, frame.area(), list_state);
+        // create the main layout
+        let layout = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(vec![Constraint::Percentage(75), Constraint::Percentage(25)])
+            .split(frame.area());
+        // set the cursor position based on the input mode
+        let mut border_style = Style::new().fg(Color::White);
+        match self.input_mode {
+            InputMode::Insert => {
+                border_style = Style::new().fg(Color::Green);
+                frame.set_cursor_position(Position {
+                    x: layout[1].x + self.input.visual_cursor() as u16 + 1,
+                    y: layout[1].y + 1,
+                });
+            }
+            InputMode::Normal => {}
+        }
+
+        // render the "stateful" List widget
+        frame.render_stateful_widget(self, layout[0], list_state);
+
+        // create a paragraph widget for the input
+        let paragraph = Paragraph::new(self.input.value())
+            .block(Block::bordered().border_set(border::THICK).title("Input"))
+            .style(border_style);
+
+        // render the paragraph widget
+        frame.render_widget(paragraph, layout[1]);
     }
 
     fn handle_events(&mut self, list_state: &mut ListState) -> io::Result<()> {
@@ -55,34 +109,65 @@ impl App {
         key_event: KeyEvent,
         list_state: &mut ListState,
     ) -> io::Result<()> {
-        if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Char('q') {
-            self.exit = true;
-        }
+        match self.input_mode {
+            InputMode::Insert => {
+                match key_event.code {
+                    KeyCode::Enter => {
+                        self.todos.push(Todo {
+                            desc: self.input.value().to_string(),
+                            done: false,
+                        });
+                        self.input.reset();
+                    }
+                    KeyCode::Esc => {
+                        self.input_mode = InputMode::Normal;
+                    }
+                    _ => {
+                        self.input
+                            .handle_event(&crossterm::event::Event::Key(key_event));
+                    }
+                }
+                return Ok(());
+            }
+            InputMode::Normal => {
+                if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Char('q') {
+                    self.exit = true;
+                }
 
-        if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Down
-            || key_event.code == KeyCode::Char('j')
-        {
-            list_state.select_next()
-        }
+                if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Down
+                    || key_event.code == KeyCode::Char('j')
+                {
+                    list_state.select_next()
+                }
 
-        if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Up
-            || key_event.code == KeyCode::Char('k')
-        {
-            list_state.select_previous()
-        }
+                if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Up
+                    || key_event.code == KeyCode::Char('k')
+                {
+                    list_state.select_previous()
+                }
 
+                if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Enter {
+                    let selected = list_state.selected().unwrap();
+                    self.todos[selected].done = !self.todos[selected].done
+                }
+
+                if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Char('e') {
+                    self.input_mode = InputMode::Insert;
+                }
+            }
+        }
         Ok(())
     }
 }
 
-#[derive(Clone)]
-struct Todo {
-    desc: String,
-    done: bool,
-}
-
 fn get_items(items: Vec<Todo>) -> Vec<String> {
-    items.iter().map(|item| format!("{}", item.desc)).collect()
+    items
+        .iter()
+        .map(|item| {
+            let done = if item.done { "✓" } else { " " };
+            format!("{} {}", item.desc, done)
+        })
+        .collect()
 }
 
 // https://docs.rs/ratatui/latest/ratatui/widgets/trait.StatefulWidget.html
@@ -90,17 +175,8 @@ impl StatefulWidget for &App {
     type State = ListState;
     fn render(self, area: Rect, buf: &mut Buffer, state: &mut ListState) {
         let title = Line::from(" Term_Todo ").centered().bold();
-        let instructions = Line::from(" Press 'q' to exit ").centered();
-        let items = [
-            Todo {
-                desc: "Item 1".to_string(),
-                done: false,
-            },
-            Todo {
-                desc: "Item 2".to_string(),
-                done: true,
-            },
-        ];
+        let instructions = Line::from(" Press 'q' to exit, Press 'e' to enter editing mode, Press 'esc' to return to normal mode ").centered();
+        let items = &self.todos;
 
         List::new(get_items(items.to_vec()))
             .block(
